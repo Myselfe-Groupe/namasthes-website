14f2957254f868081a2f52ad02e8353233afdc26
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2026-08-13 18:54:36 +0200

    Fix : site url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://myselfegroupe.com";
+  const baseUrl = "https://namasthes.myselfegroupe.com";
 
   return [
     {
