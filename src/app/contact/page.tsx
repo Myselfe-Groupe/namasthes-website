67c60c950efa6d204fe4eddaa0a20bd57ba77794
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2026-08-19 15:03:07 +0200

    Fix : sitemap url

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,13 +18,13 @@ export default function ContactPage() {
                     <div className="w-full flex flex-col gap-1 border border-secondary/20 p-4 rounded-lg bg-muted">
                         <h3 className="text-base md:text-lg font-bold">Boulangerie</h3>
                         <p>Lun-Sam : 6h00 - 21h00</p>
-                        <p>Dim : 7h00 - 21h00</p>
+                        <p>Dim : 7h00 - 13h00 / 19h00 - 21h00</p>
                     </div>
                     <div className="w-full flex flex-col gap-1 border border-secondary/20 p-4 rounded-lg bg-muted">
                         <h3 className="text-base md:text-lg font-bold">Pizzeria</h3>
                         <p>Lun-Jeu : 11h30 - 14h00 / 19h00 - 21h00</p>
                         <p>Ven-Sam : 11h30 - 14h00 / 19h00 - 21h30</p>
-                        <p>Dim : 11h30 - 14h00 / 19h00 - 21h00</p>
+                        <p>Dim : 11h30 - 13h00 / 19h00 - 21h00</p>
                     </div>
                 </div>
             </div>
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: "https://myselfe-groupe.com/sitemap.xml"
+    sitemap: "https://myselfegroupe.com/sitemap.xml"
   };
 }
